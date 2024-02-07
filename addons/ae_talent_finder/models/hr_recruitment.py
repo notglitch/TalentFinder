2ad90ec218e58491d1aeac68920d627e267bcc8c
ariadna <ahsoka@aleph.engineering>
2024-02-07 01:32:49 -0500

    [IMP]Add Method Compute employees with skills

--- addons/ae_talent_finder/models/hr_recruitment.py
+++ addons/ae_talent_finder/models/hr_recruitment.py
@@ -8,8 +8,15 @@ class TalentFinder(models.Model):
     _description = "Looking for Talents"
 
     name = fields.Char("Porpouse", help="Declare the Purpose of the search", required=True)
-    skill_ids = fields.Many2many('hr.skill.level', store=True)
-    employees_skill_ids = fields.Many2many('hr.employee.skill', store=True)
+    skill_ids = fields.Many2many('hr.skill.level', store=True, string='Skills')
+    employees_skill_ids = fields.Many2many('hr.employee.skill', string='Employees with Skills', compute='_compute_employees_with_skills', store=True)
+
+    @api.depends('skill_ids')
+    def _compute_employees_with_skills(self):
+        for talent_finder in self:
+            skill_ids = talent_finder.skill_ids.ids
+            employees_with_skills = self.env['hr.employee.skill'].search([('skill_id', 'in', skill_ids)])
+            talent_finder.employees_skill_ids = [(6, 0, employees_with_skills.ids)]
 
     def add_matches(self):
         if self.skill_ids:
